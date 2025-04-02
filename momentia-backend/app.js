5db6e677e9041c9320f01628d36f19f55ecd6f56
Erwan <erwan.a.marechal@gmail.com>
2025-04-02 11:17:36 +0200

    Update environment configuration and dependencies

--- momentia-backend/app.js
+++ momentia-backend/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
